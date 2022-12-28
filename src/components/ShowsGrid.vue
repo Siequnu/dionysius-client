dfa29b310fba18adcb260fb718d2e9cc6ca16f67
Siequnu <11821606+Siequnu@users.noreply.github.com>
2022-12-28 15:02:59 +0000

    feat(ShowsGrid): add new shows dialog

--- src/components/ShowsGrid.vue
+++ src/components/ShowsGrid.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 import ShowCard from '@/components/ShowCard.vue';
 import { useTvShowStore } from '@/stores/TvShowStore';
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
 
 const TvShowStore = useTvShowStore();
 const router = useRouter();
@@ -12,6 +15,8 @@ const goToShowPage = (show) => {
     name: 'show',
   });
 };
+
+const addNewShowDialogOpen = ref(false);
 </script>
 
 <template>
@@ -19,7 +24,17 @@ const goToShowPage = (show) => {
     <div
       class="flex flex-col gap-3 max-w-[1000px] justify-center items-center align-center"
     >
-      <h2 class="text-white text-xl p-5 font-bold">My shows</h2>
+      <div class="flex gap-3 justify-between items-center">
+        <div></div>
+        <h2 class="text-white text-xl p-5 font-bold">My shows</h2>
+        <div class="justify-end">
+          <Button
+            icon="pi pi-plus"
+            class="p-button-rounded p-button-outlined"
+            @click="addNewShowDialogOpen = true"
+          ></Button>
+        </div>
+      </div>
       <div class="flex gap-3 flex-wrap items-center justify-center">
         <ShowCard
           v-for="(show, index) in TvShowStore.shows"
@@ -30,5 +45,13 @@ const goToShowPage = (show) => {
         />
       </div>
     </div>
+
+    <Dialog
+      header="Add new show"
+      footer="Footer"
+      v-model:visible="addNewShowDialogOpen"
+    >
+      Content
+    </Dialog>
   </div>
 </template>
